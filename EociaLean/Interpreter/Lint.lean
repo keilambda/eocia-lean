158fb4bfe7098ec25537163ea798d8080e54ebec
thelissimus <thelissimus@tuta.io>
2024-01-13 17:29:53 +0500

    chore(interp-Lint): symbols

--- EociaLean/Interpreter/Lint.lean
+++ EociaLean/Interpreter/Lint.lean
@@ -1,32 +1,32 @@
-inductive Exp : Type -> Type where
-| IntE : Int -> Exp IntE
+inductive Exp : Type → Type where
+| IntE : Int → Exp IntE
 | Read : Exp IntE
-| Negate : Exp IntE -> Exp IntE
-| Minus : Exp IntE -> Exp IntE -> Exp IntE
-| Plus : Exp IntE -> Exp IntE -> Exp IntE
+| Negate : Exp IntE → Exp IntE
+| Minus : Exp IntE → Exp IntE → Exp IntE
+| Plus : Exp IntE → Exp IntE → Exp IntE
 
 open Exp
 
 structure Program (info : Type) := exp : (Exp Int)
 
-def leaf? : Exp Int -> Prop
+def leaf? : Exp Int → Prop
 | IntE _ => True
 | Read => True
 | Negate _ => False
 | Plus _ _ => False
 | Minus _ _ => False
 
-def exp? : Exp Int -> Prop
+def exp? : Exp Int → Prop
 | IntE _ => True
 | Read => True
 | Negate e => exp? e
 | Plus e1 e2 => exp? e1 ∧ exp? e2
 | Minus e1 e2 => exp? e1 ∧ exp? e2
 
-def Lint? : Program info -> Prop
+def Lint? : Program info → Prop
 | {exp} => exp? exp
 
-def interpExp : Exp Int -> IO Int
+def interpExp : Exp Int → IO Int
 | IntE i => pure i
 | Read => String.toInt! <$> (IO.getStdin >>= IO.FS.Stream.getLine)
 | Negate i => Int.neg <$> interpExp i
@@ -39,27 +39,27 @@ def interpExp : Exp Int -> IO Int
   let b ← interpExp b
   pure $ a + b
 
-def interpLint : Program info -> IO Int
+def interpLint : Program info → IO Int
 | {exp} => interpExp exp
 
-def peNegate : Exp Int -> Exp Int
+def peNegate : Exp Int → Exp Int
 | IntE i => IntE (-i)
 | other => Negate other
 
-def peMinus : Exp Int -> Exp Int -> Exp Int
+def peMinus : Exp Int → Exp Int → Exp Int
 | IntE a, IntE b => IntE (a - b)
 | a, b => Minus a b
 
-def pePlus : Exp Int -> Exp Int -> Exp Int
+def pePlus : Exp Int → Exp Int → Exp Int
 | IntE a, IntE b => IntE (a + b)
 | a, b => Plus a b
 
-def peExp : Exp Int -> Exp Int
+def peExp : Exp Int → Exp Int
 | IntE a => IntE a
 | Read => Read
 | Negate a => peNegate a
 | Minus a b => peMinus a b
 | Plus a b => pePlus a b
 
-def peLint : Program info -> Program info
+def peLint : Program info → Program info
 | {exp} => {exp := peExp exp}
