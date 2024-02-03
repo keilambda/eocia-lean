96431704fe125f6186a89651d33a542718b5f564
thelissimus <thelissimus@tuta.io>
2024-02-04 02:54:57 +0500

    initial: CVar repr

---
+++ EociaLean/Compiler/CVar.lean
@@ -0,0 +1,32 @@
+import Lean.Data.HashMap
+
+inductive Atom : Type
+| int : Int → Atom
+| var : String → Atom
+deriving Repr
+
+inductive Op : Type
+| read : Op
+| add : Atom → Atom → Op
+| sub : Atom → Atom → Op
+| neg : Atom → Op
+deriving Repr
+
+inductive Exp : Type
+| atm : Atom → Exp
+| op : Op → Exp
+deriving Repr
+
+inductive Stmt : Type
+| assign : String → Exp → Stmt
+deriving Repr
+
+inductive Tail : Type
+| ret : Exp → Tail
+| seq : Stmt → Tail → Tail
+deriving Repr
+
+abbrev Env := Lean.HashMap String Int
+
+structure CVar : Type where
+  mk :: (env : Env) (blocks : Lean.HashMap String Tail)
