7ed1bca58bb2b86feab3328cbfeecac4196b8023
thelissimus <thelissimus@tuta.io>
2024-02-06 15:08:49 +0500

    refactor(Basic): `gensym`

--- EociaLean/Basic.lean
+++ EociaLean/Basic.lean
@@ -1,7 +1,4 @@
 abbrev Var : Type := String
 abbrev Label : Type := String
 
-def gensym : StateM Nat Var := do
-  let n ← get
-  modify (· + 1)
-  pure s!"a{n}"
+def gensym : StateM Nat Var := getModify Nat.succ <&> (s!"%{·}")
