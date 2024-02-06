7329ae4a4595b7f5af5ef08b82d7712bbc4963e4
thelissimus <thelissimus@tuta.io>
2024-02-06 12:46:18 +0500

    fix: project module

--- EociaLean.lean
+++ EociaLean.lean
@@ -1,1 +1,5 @@
-import «EociaLean».Interpreter.Lint
+import EociaLean.Basic
+import EociaLean.Interpreter.LInt
+import EociaLean.Interpreter.LVar
+import EociaLean.Compiler.CVar
+import EociaLean.Compiler.x86Int
