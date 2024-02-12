3e264fa8d9d4145f6ec123a49ccd4ce374e5ca7a
thelissimus <thelissimus@tuta.io>
2024-02-12 12:33:20 +0500

    impl(x86Var): AST and `selectInstructions` nanopass

---
+++ EociaLean/Compiler/x86Var.lean
@@ -0,0 +1,137 @@
+import Std.Data.RBMap
+import EociaLean.Basic
+import EociaLean.Compiler.CVar
+
+namespace x86Var
+
+inductive Reg : Type
+| rsp | rbp | rax | rbx | rcx | rdx | rsi | rdi | r8 | r9 | r10 | r11 | r12 | r13 | r14 | r15
+deriving Repr
+
+namespace Reg
+
+instance : ToString Reg where
+  toString
+  | rsp => "rsp"
+  | rbp => "rbp"
+  | rax => "rax"
+  | rbx => "rbx"
+  | rcx => "rcx"
+  | rdx => "rdx"
+  | rsi => "rsi"
+  | rdi => "rdi"
+  | r8 => "r8"
+  | r9 => "r9"
+  | r10 => "r10"
+  | r11 => "r11"
+  | r12 => "r12"
+  | r13 => "r13"
+  | r14 => "r14"
+  | r15 => "r15"
+
+end Reg
+
+inductive Arg : Type
+| imm : Int → Arg
+| reg : Reg → Arg
+| var : Var → Arg
+| deref : Arg → Reg → Arg
+deriving Repr
+
+namespace Arg
+
+protected def toString' : Arg → String
+| imm i => s!"${toString i}"
+| reg r => s!"%{toString r}"
+| var v => v
+| deref a b => s!"{a.toString'}({b})"
+
+instance : ToString Arg where
+  toString := Arg.toString'
+
+end Arg
+
+inductive Instr : Type
+| addq : Arg → Arg → Instr
+| subq : Arg → Arg → Instr
+| negq : Arg → Instr
+| movq : Arg → Arg → Instr
+| pushq : Arg → Instr
+| popq : Arg → Instr
+| callq : Label → Int → Instr
+| retq : Instr
+| jmp : Label → Instr
+deriving Repr
+
+namespace Instr
+open Reg Arg
+
+instance : ToString Instr where
+  toString
+  | addq s d => s!"addq {s}, {d}"
+  | subq s d => s!"subq {s}, {d}"
+  | negq d => s!"negq {d}"
+  | movq s d => s!"movq {s}, {d}"
+  | pushq d => s!"pushq {d}"
+  | popq d => s!"popq {d}"
+  | callq lbl d => s!"callq {lbl}, {d}"
+  | retq => "retq"
+  | jmp lbl => s!"jmp {lbl}"
+
+def fromCVarAtom : CVar.Atom → Arg
+| CVar.Atom.int i => imm i
+| CVar.Atom.var v => var v
+
+def fromCVarOp (dest : Arg) : CVar.Op → List Instr
+| CVar.Op.read =>
+  [ callq "read_int" 0
+  , movq (reg rax) dest
+  ]
+| CVar.Op.add lhs rhs =>
+  [ movq (fromCVarAtom lhs) (reg rax)
+  , addq (fromCVarAtom rhs) (reg rax)
+  , movq (reg rax) dest
+  ]
+| CVar.Op.sub lhs rhs =>
+  [ movq (fromCVarAtom lhs) (reg rax)
+  , subq (fromCVarAtom rhs) (reg rax)
+  , movq (reg rax) dest
+  ]
+| CVar.Op.neg a =>
+  [ movq (fromCVarAtom a) (reg rax)
+  , negq (reg rax)
+  , movq (reg rax) dest
+  ]
+
+def fromCVarStmt : CVar.Stmt → List Instr
+| CVar.Stmt.assign name exp => match exp with
+  | CVar.Exp.atm a => [movq (fromCVarAtom a) (var name)]
+  | CVar.Exp.op o => fromCVarOp (var name) o
+
+def fromCVarTail : CVar.Tail → List Instr
+| CVar.Tail.ret e => match e with
+  | CVar.Exp.atm a => [movq (fromCVarAtom a) (reg rax)]
+  | CVar.Exp.op o => fromCVarOp (reg rax) o
+| CVar.Tail.seq s t => fromCVarStmt s ++ fromCVarTail t
+
+abbrev selectInstructions := fromCVarTail
+
+end Instr
+
+abbrev Env : Type := Std.RBMap Var Int compare
+
+structure Block : Type where
+  mk :: (env : Env) (instructions : List Instr)
+
+instance : ToString Block where
+  toString b := b.instructions.foldl (λ acc i => s!"{acc}\t{i}\n") default
+
+structure x86Var : Type where
+  mk :: (env : Env) (globl : Label) (blocks : Std.RBMap Label Block compare)
+
+instance : ToString x86Var where
+  toString p := s!".globl {p.globl}\n{p.blocks.foldl build default}"
+  where
+  build acc lbl block := s!"{acc}{lbl}:\n{block}"
+
+end x86Var
